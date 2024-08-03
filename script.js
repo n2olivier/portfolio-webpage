4573c460d7a0b600e17aea29afe025eaaf54af5a
n2olivier <139780882+n2olivier@users.noreply.github.com>
2024-08-03 15:11:02 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+// Toggle Menu Icon and Navbar
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+// Scroll Spy and Sticky Navbar
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('header nav a');
+const header = document.querySelector('header');
+
+window.onscroll = () => {
+    let top = window.scrollY;
+
+    // Sticky Navbar
+    header.classList.toggle('sticky', top > 100);
+
+    // Scroll Spy
+    sections.forEach(sec => {
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                if (link.getAttribute('href').includes(id)) {
+                    link.classList.add('active');
+                }
+            });
+        }
+    });
+
+    // Remove Toggle Icon and Navbar on Scroll
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
